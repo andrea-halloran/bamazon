7ad00bb328fe00a8cff1bebf141208639df092e5
Andrea Halloran <andreahalloran01@gmail.com>
2018-10-26 15:04:38 -0500

    first inquire message

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,5 +18,21 @@ function start(){
     connection.query("SELECT * FROM products", function(err, results){
         if (err) throw err; 
         console.log(results);   
+
+        inquirer
+            .prompt([
+                {
+                    name: "choice", 
+                    type: "rawlist", 
+                    choices: function(){
+                        var choiceArray = []; 
+                        for (var i =0; i < results.lenght; i ++) {
+                            choiceArray.push(results[i].item_id);
+                        }
+                        return choiceArray; 
+                    }, 
+                    message: "Which product would you like to buy? Please provide the item_id."
+                }
+            ])
     })
 }
