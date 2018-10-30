278611548989ef19b418824c8ff476de8144d76c
ahalloran <ahalloran01@gmail.com>
2018-10-29 21:24:43 -0500

    updated changes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -41,44 +41,62 @@ function start() {
             ])
             .then(function (input) {
 
-                let product = input.product;
-                let quantity = input.quantity;
+                let product = parseInt(input.product);
+                console.log(product);
+                let quantity = parseInt(input.quantity);
+                console.log(quantity);
 
-                console.log("You have selected to buy item_id: " + product + "." + '\n Please confirm that you want to purchase ' + quantity + ".");
-                inquirer.prompt([{
-                        name: "prompt",
-                        type: "confirm",
-                        message: "Confirm purchase?"
-                    }])
-                    .then(function (subtract) {
-                        // Need to figure out how to connect/disconnect from the database. 
-                        
-                        // let price = 'SELECT price FROM products WHERE item_id = ?'
-                        // connection.query(price, {item_id: product}, function (err){
-                        // if (err) throw err; 
-                        // })
 
-                        let price = connection.query('SELECT price FROM products WHERE item_id =' + product);
-                        
-                        // let dbQuantity= 'SELECT stock_quantity FROM products WHERE item_id = ?'
-                        // connection.query(dbQuantity, {item_id: quantity}, function (err){
-                        // if (err) throw err; 
-                        // })
+                // console.log("You have selected to buy item_id: " + product + "." + '\n Please confirm that you want to purchase ' + quantity + ".");
+                // inquirer.prompt([{
+                //         name: "prompt",
+                //         type: "confirm",
+                //         message: "Confirm purchase?"
+                //     }])
+                //     .then(function (subtract) {
+                // Need to figure out how to connect/disconnect from the database. 
 
-                        let dbQuantity = connection.query('SELECT stock_quantity FROM products WHERE item_id = ' + product);
+                let price = 'SELECT * FROM products WHERE item_id = ?'
+                console.log(price);
 
-                        if (dbQuantity > quantity) {
-                            console.log("*** scanning shopping cart ***")
-                            var subtract = 'UPDATE products SET stock_quantity = ' + (dbQuantity - quantity) + 'WHERE item_id = ' + product;
-                            connection.query(subtract, function (err) {
-                                if (err) throw err;
-                                console.log('Your order has been placed. Your total is $' + price * quantity);
-                            })
-                        } else {
-                            console.log("I'm sorry, we do not have enough product in stock.");
-                        }
-                    })
+                connection.query(price, [product], function (err, res) {
+                    console.log(res[0])
+                    if (err) throw err;
+                    if (res[0].stock_quantity > quantity) {
+                        console.log("*** scanning shopping cart ***")
+                        var subtract = 'UPDATE products SET stock_quantity = ? WHERE item_id = ?'
+                        connection.query(subtract, [(res[0].stock_quantity - quantity), product], function (err) {
+                            if (err) throw err;
+                            console.log('Your order has been placed. Your total is $' + res[0].price * quantity);
+                            console.log(res[0].stock_quantity);
+                        })
+                    } else {
+                        console.log("I'm sorry, we do not have enough product in stock.");
+                    }
 
+                })
             })
+
+        // let price = connection.query('SELECT price FROM products WHERE item_id =' + product);
+
+        // let dbQuantity = 'SELECT stock_quantity FROM products WHERE item_id = ?'
+        // connection.query(dbQuantity, [quantity], function (err, res) {
+        //     if (err) throw err;
+
+        // })
+
+        // let dbQuantity = connection.query('SELECT stock_quantity FROM products WHERE item_id = ' + product);
+
+        // if (dbQuantity > quantity) {
+        //     console.log("*** scanning shopping cart ***")
+        //     var subtract = 'UPDATE products SET stock_quantity = ' + (dbQuantity - quantity) + 'WHERE item_id = ' + product;
+        //     connection.query(subtract, function (err) {
+        //         if (err) throw err;
+        //         console.log('Your order has been placed. Your total is $' + price * quantity);
+        //     })
+        // } else {
+        //     console.log("I'm sorry, we do not have enough product in stock.");
+        // }
     })
+
 }
